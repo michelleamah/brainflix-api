49006c631177287bdfc47f86295118325d8251f8
Michelle Mah <michellemah2001@gmail.com>
2024-05-19 01:40:10 -0400

    Fix upload functionality

--- routes/videos.js
+++ routes/videos.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require('uuid'); 
 
-const videoFilePath = path.join(__dirname, '../data/videos.json');
+const videoFilePath = path.join(__dirname, "../data/videos.json");
 
 const readVideoFile = (callback) => {
-  fs.readFile(videoFilePath, 'utf8', (err, data) => {
+  fs.readFile(videoFilePath, "utf8", (err, data) => {
     if (err) {
       callback(err, null);
       return;
@@ -15,13 +16,23 @@ const readVideoFile = (callback) => {
   });
 };
 
-router.get('/', (req, res) => {
-  readVideoFile((err, data) => {
+const writeVideoFile = (videos, callback) => {
+  fs.writeFile(videoFilePath, JSON.stringify({ videos }), (err) => {
     if (err) {
-      res.status(500).send('Error reading video data');
+      callback(err);
       return;
     }
-    const videos = data.videos.map(video => ({
+    callback(null);
+  });
+};
+
+router.get("/", (req, res) => {
+  readVideoFile((err, data) => {
+    if (err) {
+      res.status(500).send("Error reading video data");
+      return;
+    }
+    const videos = data.videos.map((video) => ({
       id: video.id,
       title: video.title,
       channel: video.channel,
@@ -31,19 +42,57 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const videoId = req.params.id;
   readVideoFile((err, data) => {
     if (err) {
-      res.status(500).send('Error reading video data');
+      res.status(500).send("Error reading video data");
       return;
     }
-    const video = data.videos.find(v => v.id === videoId);
+    const video = data.videos.find((v) => v.id === videoId);
     if (video) {
       res.json(video);
     } else {
-      res.status(404).send('Video not found');
+      res.status(404).send("Video not found");
     }
+  });
+});
+
+router.post("/", (req, res) => {
+  const { title, description } = req.body;
+
+  readVideoFile((err, data) => {
+    if (err) {
+      res.status(500).send("Error reading video data");
+      return;
+    }
+
+    const videos = data.videos;
+
+    const newVideo = {
+      id: uuidv4(),
+      title,
+      description,
+      channel: "Michelle Mah",
+      image: "http://localhost:3000/images/upload-video-preview.jpg",
+      views: "0", 
+      likes: "0", 
+      duration: "5:26",
+      video: "", 
+      timestamp: Date.now(),
+      comments: [],
+    };
+
+    videos.push(newVideo);
+
+    writeVideoFile(videos, (err) => {
+      if (err) {
+        res.status(500).send("Error writing video data");
+        return;
+      }
+
+      res.status(201).json(newVideo);
+    });
   });
 });
 
